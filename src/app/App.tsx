9f34933c521d6c43d5ad0562706c1c56bcdfe8a8
Rypin <hhgfcswd@gmail.com>
2025-08-24 16:09:10 +0300

    feat: создал страницу home и вынес в нее разметку

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,31 +1,10 @@
-import {NewsApiService} from "../entities/news/api/newsApiService.ts";
-import {useEffect, useState} from "react";
-import type {TNewsApiArticles} from "../entities/news/api/newsapi.types.ts";
-import Container from "./Container/Container.tsx";
-import Link from "../shared/UI/Link/Link.tsx";
-import Header from "../widgets/Header/Header.tsx";
+import Home from "../pages/Home.tsx";
 
 function App() {
     document.documentElement.setAttribute('data-theme', 'dark')
 
-    const [newsData, setNewsData] = useState<TNewsApiArticles[]>([])
-
-    // useEffect(() => {
-    //     NewsApiService.topHeadlines({q: 'trump'}).then((data) => {
-    //         if (data.status === 'ok') setNewsData(data.articles)
-    //         else console.log(data)
-    //     })
-    // }, [])
-
     return (
-        <>
-            <Header></Header>
-            <Container>
-                <Link href={newsData.length > 0 ? newsData[0].url : '#'}>
-                    {newsData.length > 0 ? newsData[0].title : 'Loading...'}
-                </Link>
-            </Container>
-        </>
+        <Home></Home>
     )
 }
 
---
+++ src/pages/Home.tsx
@@ -0,0 +1,18 @@
+import Header from "../widgets/Header/Header.tsx";
+import Container from "../widgets/Container/Container.tsx";
+
+function Home() {
+    return (
+        <>
+            <Header></Header>
+            <main>
+                <Container>
+                    work
+                </Container>
+            </main>
+            <footer></footer>
+        </>
+    );
+}
+
+export default Home;
