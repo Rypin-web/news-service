705804d12858826ad137825b54efe2ad6ef0f04d
Rypin <hhgfcswd@gmail.com>
2025-08-24 17:10:38 +0300

    feat: слайс для работы с локалью и данными для ui

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,14 @@
 import {configureStore} from "@reduxjs/toolkit";
 import {newsApi} from "../entities/news/newsApi.ts";
+import {localeSlice} from "../shared/locale/localeSlice.ts";
 
 export const store = configureStore({
     reducer: {
+        locale:localeSlice.reducer,
         [newsApi.reducerPath] : newsApi.reducer
     },
     middleware: (gDM) =>
         gDM().concat(newsApi.middleware)
 })
 
+export type TRootState = ReturnType<typeof store.getState>
---
+++ src/shared/locale/localeSlice.ts
@@ -0,0 +1,31 @@
+import {createSlice} from "@reduxjs/toolkit";
+import {supportedLocales, type TLocale, type TLocaleStructure, translations} from "./data.ts";
+import {getBrowserLocale} from "../utils/getBrowserLocale.ts";
+
+type TPayloadChangeLocale = {
+    payload: TLocale
+}
+
+type TInitialState = {
+    locale: TLocale,
+    translations: Partial<TLocaleStructure<TLocale>>
+}
+
+const initialLocale = getBrowserLocale()
+const initialState: TInitialState = {
+    locale: initialLocale,
+    translations: translations[initialLocale]
+}
+
+export const localeSlice = createSlice({
+    name: 'localeSlice',
+    initialState,
+    reducers: {
+        changeLocale: (state: TInitialState, {payload}: TPayloadChangeLocale) => {
+            if (supportedLocales.includes(payload)) {
+                state.locale = payload
+                state.translations = translations[payload]
+            }
+        }
+    }
+})
