40c663a30f790178b423ffecc32ed57aa68bc1f7
Rypin <hhgfcswd@gmail.com>
2025-08-24 18:19:03 +0300

    feat: компонент иконок

---
+++ src/shared/UI/Icon/Icon.tsx
@@ -0,0 +1,17 @@
+import {iconsData} from "./IconsData.ts";
+
+type TIconProps = {
+    name: keyof typeof iconsData,
+    w?:number,
+    h?:number,
+}
+
+function Icon({name, w = 24, h = 24}:TIconProps) {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" height={h} viewBox="0 -960 960 960" width={w} fill='inherit'>
+            <path d={iconsData[name]}/>
+        </svg>
+    );
+}
+
+export {Icon};
