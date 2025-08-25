5c9629c47a96ed69f24e8715fec7814703dcaa61
Rypin <hhgfcswd@gmail.com>
2025-08-25 16:54:44 +0300

    feat: создал хук для работы со слайсом параметров
    - верстка временная в шапке
    - для теста

---
+++ src/shared/hooks/useNewsParams.ts
@@ -0,0 +1,17 @@
+import {useDispatch} from "react-redux";
+import {useTypedSelector} from "./useTypedSelector.ts";
+import {useCallback} from "react";
+import {newsParamsSlice} from "../slices/newsParams/newsParamsSlice.ts";
+import type {TNewsEverythingParams, TNewsHeadlinesParams} from "../../entities/news/newsapi.types.ts";
+
+export function useNewsParams<T extends 'everything' | 'topHeadlines'>(key: T) {
+    const dispatch = useDispatch()
+    const params = useTypedSelector((state) => state.newsParams[key])
+    const setParams = useCallback(
+        (params: T extends 'everything' ? TNewsEverythingParams : TNewsHeadlinesParams) => {
+        if (key === 'everything') dispatch(newsParamsSlice.actions.setEverythingParams(params as TNewsEverythingParams))
+        if (key === 'topHeadlines') dispatch(newsParamsSlice.actions.setTopHeadlinesParams(params))
+    }, [dispatch])
+
+    return {params, setParams}
+}
--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -3,14 +3,28 @@ import Link from "../../shared/UI/Link/Link.tsx";
 import Locale from "../../features/Locale/Locale.tsx";
 import {ToggleTheme} from "../../features/ToggleTheme/ToggleTheme.tsx";
 import {useTranslations} from "../../shared/hooks/useTranslations.ts";
+import {useNewsParams} from "../../shared/hooks/useNewsParams.ts";
+import {useTopHeadlinesQuery} from "../../entities/news/newsApi.ts";
+import {useRef} from "react";
 
 function Header() {
     const uiData = useTranslations()
+    const {params, setParams} = useNewsParams('topHeadlines')
+    const {isLoading, isSuccess, data} = useTopHeadlinesQuery(params)
+    const input = useRef<HTMLInputElement>(null)
+
+    const search = () => {
+        setParams({...params, q:input.current?.value})
+    }
+
+    console.log(data)
 
     return (
         <header className={cl.header}>
             <Link href={'#'}><h2>{uiData.logo}</h2></Link>
             {/*search*/}
+            <input type='search' ref={input}/>
+            <button onClick={search}>search</button>
             <div></div>
             <div></div>
             <div></div>
