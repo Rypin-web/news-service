2bd4a50fd8af64f4b5d59d6fc33e26cb224c336a
Rypin <hhgfcswd@gmail.com>
2025-08-26 21:38:51 +0300

    feat: новые пропсы и новая иконка в дате

--- src/shared/UI/IconButton/IconButton.tsx
+++ src/shared/UI/IconButton/IconButton.tsx
@@ -1,13 +1,18 @@
 import cl from './IconButton.module.css'
 import {Icon, type TIconProps} from "../Icon/Icon.tsx";
+import {type FormEvent, type ReactNode} from "react";
 
 type TIconButtonProps = TIconProps & {
-    handleClick?: () => void
+    children?: ReactNode
+    className?: string,
+    type?: "button" | "submit" | "reset",
+    handleClick?: (e: FormEvent<HTMLButtonElement>) => void,
 }
 
 function IconButton(p: TIconButtonProps) {
     return (
-        <button onClick={p?.handleClick} className={cl.iconButton}>
+        <button type={p.type ? p.type : 'button'} onClick={p?.handleClick} className={`${cl.iconButton} ${p.className}`}>
+            {p.children && p.children}
             <Icon name={p.name} w={p.w} h={p.h} />
         </button>
     );
