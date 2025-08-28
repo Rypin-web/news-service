9abb3e34c94d6bc50ae373984e2b87642d719f2b
Rypin <hhgfcswd@gmail.com>
2025-08-28 12:18:04 +0300

    refactor: вынес установку атрибута за функцию
    - при изменении языка компонент ререндерился и атрибут снова ставился

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,10 @@
 import Home from "../pages/Home.tsx";
 import {useTranslations} from "../shared/hooks/useTranslations.ts";
 
+document.documentElement.setAttribute('data-theme', 'dark')
+
 function App() {
     const uiData = useTranslations()
-    document.documentElement.setAttribute('data-theme', 'dark')
     document.title = uiData.tab_title ? uiData.tab_title : 'Loading..'
 
     return (
